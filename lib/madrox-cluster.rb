c1620fb23d0cd3def610b410da0f898eab084f01
Iuri Matias <iuri.matias@gmail.com>
2013-09-08 22:09:41 +0200

    support for execute, map

--- lib/madrox-cluster.rb
+++ lib/madrox-cluster.rb
@@ -25,7 +25,70 @@ module Madrox
     server
     #@@hosts.sample
   end
-  
+
+  def self.execute(&block)
+    host = self.get_next_free_host
+    connection = TCPSocket.new(host[:hostname], host[:port]) 
+
+    package = {type: "execute", code: block.to_source, args: []}.to_json
+
+    connection.sendmsg package
+
+    puts "finished sending msg"
+    result = ""
+    if line = connection.gets
+      result << line
+      puts "received #{line.chop}"
+    end
+
+    connection.close
+    puts "finished receiving msg"
+    eval(result.chop)
+  end
+
+  def self.each(array, options = {}, &block)
+    array = array.to_a # force Enumerables into an Array
+
+    #create connections first to avoid race conditions
+    #NOTE: perhaps connections can be reused..., with concurrency this can be
+    #tricky..
+    connections = array.collect do |x|
+      host = self.get_next_free_host
+      TCPSocket.new(host[:hostname], host[:port]) 
+    end
+
+    res = Parallel.each_with_index(array, :in_threads => array.size) do |x, index|
+      #TODO: ideal would be to delay this until there is a 'free' server
+      connection = connections[index]
+
+      begin
+        package = {type: "execute", code: block.to_source, args: [x]}.to_json
+
+        connection.sendmsg package # + "|||"
+      rescue
+        puts "error: retrying.."
+        retry
+      end
+
+      puts "finished sending msg"
+      result = ""
+      if line = connection.gets
+        result << line
+        puts "received #{line.chop}"
+      end
+
+      connection.close
+      puts "finished receiving msg"
+      eval(result.chop)
+    end
+
+    res
+  end
+
+  def self.map(array, options={}, &block)
+    self.collect(array, options, &block)
+  end
+
   def self.collect(array, options = {}, &block)
     array = array.to_a # force Enumerables into an Array
 
@@ -41,9 +104,11 @@ module Madrox
     res = Parallel.map_with_index(array, :in_threads => array.size) do |x, index|
       #TODO: ideal would be to delay this until there is a 'free' server
       connection = connections[index]
-      
+
       begin
-        connection.sendmsg block.to_source # + "|||"
+        package = {type: "execute", code: block.to_source, args: [x]}.to_json
+
+        connection.sendmsg package # + "|||"
       rescue
         puts "error: retrying.."
         retry
--- lib/madrox-cluster/server.rb
+++ lib/madrox-cluster/server.rb
@@ -11,20 +11,26 @@ module Madrox
       #send_data "free"
     end
 
+    #possible calls
+    # execute
+    # register
+
     #TODO: make this non blockable
     def receive_data data
       #send_data "busy"
       packages = data.split("|||")
+      puts "got"
       puts packages
       packages.each do |package|
-        Thread.new {
-          result = eval(package).call()
+        #Thread.new {
+          #binding.pry
+          data = JSON.parse(package)
+          args = data["args"].size == 1 ? data["args"].first : data["args"]
+          result = eval(data["code"]).call(args)
           puts result
           send_data result.to_s + "\n"
-          #send_data "free\n"
           puts "sending msg... #{result}"
-          #close_connection if data =~ /quit/i
-        }
+        #}
       end
     end
 
