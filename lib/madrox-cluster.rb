cf9197c77be6e856ce175d3a02d482083bfc2851
Iuri Matias <iuri.matias@gmail.com>
2013-11-10 18:06:05 +0100

    refactor using more idiomatic class << self

--- lib/madrox-cluster.rb
+++ lib/madrox-cluster.rb
@@ -12,35 +12,38 @@ require "madrox-cluster/workers"
 
 module Madrox
 
-  def self.config(host_list)
-    HostsManager.add_hosts(host_list)
-  end
+  class << self
 
-  def self.register(reference, code)
-    HostsManager.send_to_all JsonPackage.register(reference, code)
-  end
+    def config(host_list)
+      HostsManager.add_hosts(host_list)
+    end
 
-  def self.execute(&block)
-    host = HostsManager.get_next_free_host
-    response = host.send JsonPackage.execute(block)
-    result = JsonPackage.parse(response)
-    eval(result.result.to_s)
-  end
+    def register(reference, code)
+      HostsManager.send_to_all JsonPackage.register(reference, code)
+    end
 
-  def self.each(array, options = {}, &block)
-    self.collect(array, options, &block)
-    nil
-  end
+    def execute(&block)
+      host = HostsManager.get_next_free_host
+      response = host.send JsonPackage.execute(block)
+      result = JsonPackage.parse(response)
+      eval(result.result.to_s)
+    end
 
-  def self.map(array, options={}, &block)
-    self.collect(array, options, &block)
-  end
+    def each(array, options = {}, &block)
+      collect(array, options, &block)
+      nil
+    end
 
-  def self.collect(array, options = {}, &block)
-    workers = Workers.new(array, block)
-    result  = workers.execute
-    
-    return result
-  end
+    def map(array, options={}, &block)
+      collect(array, options, &block)
+    end
 
+    def collect(array, options = {}, &block)
+      workers = Workers.new(array, block)
+      result  = workers.execute
+      
+      return result
+    end
+
+  end
 end
