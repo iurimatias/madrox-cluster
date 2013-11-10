935093f42c4a866d6ad477bdd9641d102557672c
Iuri Matias <iuri.matias@gmail.com>
2013-11-10 18:03:47 +0100

    refactor json_builder using more idiomatic class << self

--- lib/madrox-cluster/json_builder.rb
+++ lib/madrox-cluster/json_builder.rb
@@ -1,22 +1,24 @@
 module Madrox
   class JsonPackage
+    class << self
 
-    def self.register(reference, code)
-      {:type => "register", :reference => reference, :code => code.to_source}.to_json
+      def register(reference, code)
+        {:type => "register", :reference => reference, :code => code.to_source}.to_json
+      end
+
+      def execute(block, args=[])
+        {:type => "execute", :code => block.to_source, :args => args}.to_json
+      end
+
+      def result(result)
+        {:type => "result", :result => result}.to_json
+      end
+
+      def parse(package)
+        json = JSON.parse(package)
+        OpenStruct.new(json)
+      end
+
     end
-
-    def self.execute(block, args=[])
-      {:type => "execute", :code => block.to_source, :args => args}.to_json
-    end
-
-    def self.result(result)
-      {:type => "result", :result => result}.to_json
-    end
-
-    def self.parse(package)
-      json = JSON.parse(package)
-      OpenStruct.new(json)
-    end
-
   end
 end
