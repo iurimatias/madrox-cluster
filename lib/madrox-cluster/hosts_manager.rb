03d35c70d94875b22ef5e76b2ab5a8af263f081b
Iuri Matias <iuri.matias@gmail.com>
2013-11-10 18:00:03 +0100

    refactor hosts manager using more idiomatic class << self

--- lib/madrox-cluster/hosts_manager.rb
+++ lib/madrox-cluster/hosts_manager.rb
@@ -1,32 +1,36 @@
 module Madrox
   class HostsManager
 
-    def self.add_hosts(host_list)
-      host_list.each do |host|
-        hostname, port = host.split(":")
-        add_host(hostname, port)
+    class << self
+
+      def add_hosts(host_list)
+        host_list.each do |host|
+          hostname, port = host.split(":")
+          add_host(hostname, port)
+        end
       end
-    end
 
-    def self.add_host(hostname, port)
-      @@hosts ||= []
-      @@hosts << Host.new(hostname, port)
-    end
+      def add_host(hostname, port)
+        @@hosts ||= []
+        @@hosts << Host.new(hostname, port)
+      end
 
-    def self.get_next_free_host
-      @@hosts.min_by { |h| h.jobs }
-    end
+      def get_next_free_host
+        @@hosts.min_by { |h| h.jobs }
+      end
 
-    def self.get_free_hosts()
-      @@hosts.select { |host| host.jobs == 0 }
-    end
+      def get_free_hosts()
+        @@hosts.select { |host| host.jobs == 0 }
+      end
 
-    def self.send_to_all(payload)
-      @@hosts.each { |host| host.send payload, false }
-    end
+      def send_to_all(payload)
+        @@hosts.each { |host| host.send payload, false }
+      end
 
-    def self.close_connections
-      @@hosts.map(&:close_connection)
+      def close_connections
+        @@hosts.map(&:close_connection)
+      end
+
     end
 
   end
